4182ef2f07f29951653aef311e650c2167f3679c
smurphy68 <smurphy68@live.co.uk>
2026-06-06 17:39:36 +0100

    try to get python logs

--- irrigation.sh
+++ irrigation.sh
@@ -6,6 +6,7 @@ After=network-online.target
 Wants=network-online.target
 
 [Service]
+Environment=PYTHONUNBUFFERED=1
 ExecStartPre=/usr/bin/pip install -r /home/van/Documents/smart-irrigator/smart-irrigator/requirements.txt --break-system-packages
 ExecStart=/usr/bin/python3 /home/van/Documents/smart-irrigator/smart-irrigator/main.py
 EnvironmentFile=/home/van/Documents/smart-irrigator/smart-irrigator/.env
